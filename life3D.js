1e603baba516f44221cdf6585c06d8bd906b07da
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-05-28 18:36:40 -0400

    use updated (and minified) easyCam
    no longer need the easyCamFix workaround, nice

--- life3D.js
+++ life3D.js
@@ -15,7 +15,6 @@ function setup() {
   grid = new Grid(width, height);
   grid.init("3D");
 
-  easyCamFix();
   easycam = new Dw.EasyCam(this._renderer, {
     distance: 400,
   });
@@ -111,29 +110,6 @@ function lighting() {
   pointLight(175, 175, 175, locX, locY, 250);
   pointLight(175, 175, 175, locX, locY, -250);
   colorMode(HSB);
-}
-
-function easyCamFix() {
-  // fix for EasyCam to work with p5 v0.9.0+
-  Dw.EasyCam.prototype.apply = function (n) {
-    let o = this.cam;
-    (n = n || o.renderer),
-      n &&
-        ((this.camEYE = this.getPosition(this.camEYE)),
-        (this.camLAT = this.getCenter(this.camLAT)),
-        (this.camRUP = this.getUpVector(this.camRUP)),
-        n._curCamera.camera(
-          this.camEYE[0],
-          this.camEYE[1],
-          this.camEYE[2],
-          this.camLAT[0],
-          this.camLAT[1],
-          this.camLAT[2],
-          this.camRUP[0],
-          this.camRUP[1],
-          this.camRUP[2]
-        ));
-  };
 }
 
 // DOM stuff
